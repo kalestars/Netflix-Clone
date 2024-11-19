fd965854e33dee22465625f2a6f6a52b47b22c5e
Sara Ali <kalestars@gmail.com>
2024-11-19 13:55:50 +0300

    add banner

--- netflix-clone/src/Pages/Home/Home.jsx
+++ netflix-clone/src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../Header/Header'
 import Footer from '../../Footer/Footer'
+import Banner from '../../Banner/Banner'
 
 
 const Home = () => {
   return (
     <>
        <Header />
+       <Banner />
        <Footer />
     </>
   )
